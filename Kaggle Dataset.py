82d1780effc5e3fb9ca50bdcb4be773d77d46e15
Miguel Manalili <manalili.mig@gmail.com>
2025-09-08 21:51:57 +0100

    Importing Dataset 2

--- Kaggle Dataset.py
+++ Kaggle Dataset.py
@@ -1,6 +1,11 @@
 import kagglehub
 
-# Download latest version
+# Video Games Dataset
 path = kagglehub.dataset_download("gregorut/videogamesales")
+
+print("Path to dataset files:", path)
+
+# Starbucks Dataset
+path = kagglehub.dataset_download("starbucks/store-locations")
 
 print("Path to dataset files:", path)
